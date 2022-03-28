f14da179aa896b72cb9b833a9c985992e1c01fa7
MicaelaCuan2 <micucuan@gmail.com>
2022-03-28 11:42:49 -0300

    Ejercicios del 1 al 5 actualizados

---
+++ Assets/Ejercicio2.cs
@@ -0,0 +1,24 @@
+﻿/*Realizá un programa que, siendo num1 y num2 variables enteras  con valores 4 y 5, respectivamente, realice la operación correspondiente y muestre el resultado en pantalla.
+a)Suma
+b)Producto
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        Debug.Log("La suma entre ambos números es " + (num1 + num2) + " El producto entre los dos números es " + (num1 * num2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio5.cs
@@ -0,0 +1,31 @@
+﻿// Realizá un programa que, dados dos números enteros num1 y num2 cargados desde  el Inspector muestre el resultado de la división del primero por el segundo en un texto UI siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.  
+//En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:  “No se puede dividir por cero”. 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num2 == 0)
+        {
+            Debug.Log("Por favor ingrese un número válido, que no sea 0");
+        }
+        else
+        {
+            double res = System.Convert.ToDouble(num1) / System.Convert.ToDouble(num2);
+            Debug.Log("El resultado de la división " + num1 + ":" + num2 + " es " + res);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
